021faa8b46085ee8ceac969fb8a4c79b108c532c
William Gyasi <kwabenagyasi63@gmail.com>
2020-09-21 20:25:13 +0000

    Div CHange Over

--- Cake/js/carousel.js
+++ Cake/js/carousel.js
@@ -24,22 +24,87 @@ const moveToSlide=(track,currentSlide,targetSlide)=>{
     currentSlide.classList.remove('current-slide')
     targetSlide.classList.add('current-slide')
 }
+
+const updateDot=(currentDot,targetDot)=>{
+    
+    currentDot.classList.remove('current-slide')
+    targetDot.classList.add ('current-slide')
+
+}
+
+const hideShowArrows =(slides,prevButton,nextButton,targetIndex)=>{
+    if(targetIndex===0){
+        prevButton.classList.add('is-hidden')
+        nextButton.classList.remove('is-hidden')
+    }
+    else if(targetIndex===slides.length-1){
+        prevButton.classList.remove('is-hidden')
+        nextButton.classList.add('is-hidden')
+    }
+    else{
+        prevButton.classList.remove('is-hidden')
+        nextButton.classList.remove('is-hidden')
+    }
+
+}
 //WHEN RIGHT BUTTON IS CLICKED
 nextButton.addEventListener('click',e=>{
     //MOVE THE SLIDE
     const currentSlide=track.querySelector('.current-slide')
     const nextSlide=currentSlide.nextElementSibling
+    const currentDot = dotsNavs.querySelector('.current-slide')
+    const nextDot=currentDot.nextElementSibling
+
+    const nextIndex=slides.findIndex(slide=>slide===nextSlide)
+
+
     moveToSlide(track,currentSlide,nextSlide)
+    updateDot(currentDot,nextDot) 
+
+    hideShowArrows(slides,prevButton,nextButton,nextIndex)
+
+
 })
 
 prevButton.addEventListener('click',e=>{
     //MOVE THE SLIDE
     const currentSlide=track.querySelector('.current-slide')
     const prevSlide=currentSlide.previousElementSibling
+    const currentDot = dotsNavs.querySelector('.current-slide')
+    const previosDot=currentDot.previousElementSibling
+    
+    const nextIndex=slides.findIndex(slide=>slide===prevSlide)
+
+    
+
+
     moveToSlide(track,currentSlide,prevSlide)
+    updateDot(currentDot,previosDot) 
+
+    hideShowArrows(slides,prevButton,nextButton,nextIndex)
+
 
 })
 
 dotsNavs.addEventListener('click',event=>{
+    //WHAT INIDCATOR WAS CLICKED
+    const targetDot =event.target.closest('button')
+    if(!targetDot) return;
+
+    const currentSlide =track.querySelector('.current-slide')
+    const currentDot = dotsNavs.querySelector('.current- slide')
+
+    const targetIndex=dots.findIndex(element=>element===targetDot)
+
+    const targetSlide =slides[targetIndex]
+
+    moveToSlide(track,currentSlide,targetSlide)
+
+    updateDot(currentDot,targetDot)
+
+    hideShowArrows(slides,prevButton,nextButton,targetIndex)
+
+    
+
     
 })
